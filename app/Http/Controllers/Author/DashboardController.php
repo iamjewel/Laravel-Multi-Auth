e708ccc6aab1473f9bf6e6b6f4bf690bde8b4401
IronySoft <ironyoft.ltd@gmail.com>
2019-08-28 12:26:18 +0600

    Admin & Author Route Group Created

--- app/Http/Controllers/Author/DashboardController.php
+++ app/Http/Controllers/Author/DashboardController.php
@@ -7,5 +7,7 @@ use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(){
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Admin Routes
+Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']],
+    function () {
+
+        Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+    });
+
+//Author Routes
+Route::group(['as' => 'author.', 'prefix' => 'author', 'namespace' => 'Author', 'middleware' => ['auth', 'author']],
+    function () {
+
+        Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+    });
